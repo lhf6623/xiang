36245e3415295d8219fc00adaaf945929ad7a591
lhf6623 <1336955219@qq.com>
2023-06-19 16:07:02 +0800

    add: less

--- src/components/PanelBox.tsx
+++ src/components/PanelBox.tsx
@@ -1,7 +1,7 @@
 export default () => {
   const top_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const button_arr = ["九", "八", "七", "六", "五", "四", "三", "二", "一"];
-  const content_arr = Array(90).fill(0);
+  const content_arr = Array(90).fill('');
   // w-230 h-290
   return (
     <div flex shrink-0 flex-col items-center m-border w-232px bg="#eed3b3">
@@ -14,7 +14,7 @@ export default () => {
       </div>
       <div w-225px flex-1 flex flex-wrap relative class="m-after-panel">
         {content_arr.map((item) => (
-          <span w-25px h-25px text-center leading-25px>
+          <span w-25px h-25px text-center leading-25px class="gz">
             {item}
           </span>
         ))}
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { render } from "solid-js/web";
 import '@unocss/reset/tailwind-compat.css'
 import 'virtual:uno.css'
 
-import '@/styles/index.css'
+import '@/styles/index.less'
 import App from "@/App";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
